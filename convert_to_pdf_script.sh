a821070106c1738dc81c92c95140c373c80d765b
7erminal <bede.abbe@gmail.com>
2024-01-29 11:02:56 +0000

    changes

--- convert_to_pdf_script.sh
+++ convert_to_pdf_script.sh
@@ -2,21 +2,21 @@
 
 sudo rm -rf temp_dir
 sudo mkdir temp_dir
-sudo cd temp_dir
-sudo cp media/* .
+cd temp_dir
+sudo cp ../media/* .
 #cd ..
 
-directory="temp_dir"
+# directory="temp_dir"
 
-for file in "$directory"/*; do
+for file in *; do
 	if [ $(head -c 4 "$file") = "%PDF" ]; then
     		echo "It's a PDF file. Skip"
-		sudo rm media/"$file"
+		sudo rm "$file"
 	else
 		# soffice --headless --convert-to pdf:writer_pdf_Export --outdir . "$file"
 		sudo libreoffice7.6 --headless --convert-to pdf:writer_pdf_Export --outdir . "$file"
-		sudo rm "$directory"/"$file"
-		sudo rm media/"$file"
+		sudo rm "$file"
+		sudo rm ../media/"$file"
 	fi
 done
 
